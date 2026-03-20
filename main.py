f879ed907403fa980f71565dbd3624585c925bda
juancontreras1145 <48428486+juancontreras1145@users.noreply.github.com>
2026-03-20 01:15:42 -0300

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,60 +1,26 @@
-from machine import Pin, PWM
+from machine import Pin
 import time
 
-print("IR EMISOR 38kHz INICIADO")
+print("MODO CAPTURA IR")
 
-# ===== CONFIG =====
-IR_PIN = 5
-CARRIER = 38000
-DUTY = 512   # 50%
-
-# ===== PWM IR =====
-pwm = PWM(Pin(IR_PIN))
-pwm.freq(CARRIER)
-pwm.duty(DUTY)
-
-# ===== SEÑAL CAPTURADA (TU BOTON) =====
-signal = [
-240,35,75,3319,215,7634,219,1994,197,1958,389,2034,156,1795,403,750,
-477,970,212,1722,443,840,348,696,423,724,485,872,280,1823,443,1748,
-497,1795,457,641,498,1747,593,1646,484,1769,498,1733,484,799
-]
-
-def mark(t):
-    pwm.duty(DUTY)
-    time.sleep_us(t)
-
-def space(t):
-    pwm.duty(0)
-    time.sleep_us(t)
-
-def send_ir(data):
-
-    mark_state = True
-
-    for t in data:
-
-        if mark_state:
-            mark(t)
-        else:
-            space(t)
-
-        mark_state = not mark_state
-
-    pwm.duty(0)
-
+ir = Pin(15, Pin.IN)
 
 while True:
 
-    print("Enviando señal IR")
+    last = ir.value()
+    pulses = []
+    start = time.ticks_us()
 
-    # repetir 3 veces
-    send_ir(signal)
-    time.sleep_ms(40)
-    send_ir(signal)
-    time.sleep_ms(40)
-    send_ir(signal)
+    while time.ticks_diff(time.ticks_us(), start) < 500000:
 
-    print("Señal enviada")
+        v = ir.value()
 
-    time.sleep(5)
+        if v != last:
+            now = time.ticks_us()
+            pulses.append(time.ticks_diff(now, start))
+            start = now
+            last = v
+
+    if len(pulses) > 20:
+        print("CAPTURADO:")
+        print(pulses)
